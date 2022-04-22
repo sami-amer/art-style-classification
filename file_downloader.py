ec1cf1101365087007470338b99cf4c67a137345
sami-amer <59567234+samiatMIT@users.noreply.github.com>
2022-04-22 05:05:02 -0400

    updated sylized.ipynb to also allow training of a base ImageNet mode, ConvNext

--- file_downloader.py
+++ file_downloader.py
@@ -1,21 +1,63 @@
 import requests
 import json
 import wget
+import pandas as pd
+import pickle
+
+# df = pd.read_csv("MetObjects.csv")
+
+
+# with open(r"MetObjects.pickle", "wb") as output_file:
+#      pickle.dump(df, output_file)
+
+with open(r"MetObjects.pickle", "rb") as input_file:
+    df = pickle.load(input_file)
+print(df.columns)
+
+# periods = df["Period"].value_counts()
+# cultures = df["Culture"].value_counts()
+# dynasty = df["Dynasty"].value_counts()
+department = df["Department"].value_counts()
+# classification = df["Classification"].value_counts()
+# object_names = df["Object Name"].value_counts()
+
+edo = df.loc[df['Department'] == "Drawings and Prints"]
+
+edo_prints = edo.loc[edo['Object Name'] == "Print"]
+edo_prints_by_ident = edo_prints["Locale"].value_counts()
+
+print(edo_prints_by_ident)
+# print(edo_prints["Object ID"])
+
+# object_names_edo = edo["Object Name"].value_counts()
+# print(object_names_edo)
+
+# print(edo_prints["Classification"].value_counts())
+# print(edo_prints["Culture"].value_counts())
+
+# print(department)
+
+# for index,item in dynasty.iteritems():
+#     if item > 5000:
+#         print(index,item)
+
+
 if __name__ == '__main__':
-    url = "https://collectionapi.metmuseum.org/public/collection/v1/objects/35972"
-    r = requests.get(url)
-    # print(r)
-    # print(r.json())
-    dic = r.json()
-    image_url = dic["primaryImage"]
-    # print(dic[0])
-    wget.download(image_url,out='test_folder/test_name.jpg')
+    # url = "https://collectionapi.metmuseum.org/public/collection/v1/objects/36461"
+    # r = requests.get(url)
+    # # # print(r)
+    # # # print(r.json())
+    # dic = r.json()
+    # image_url = dic["primaryImage"]
+    # # # print(dic[0])
+    # wget.download(image_url,out='image_folder/test_name.jpg')
 
 
-    import time
-    list_to_loop = [0]*100
+    # import time
+    # list_to_loop = [0]*100
 
-    for i,item in enumerate(list_to_loop):
-        if i%80 == 0:
-            time.sleep(1)
-        # Rest of code here
+    # for i,item in enumerate(list_to_loop):
+    #     if i%80 == 0:
+    #         time.sleep(1)
+    #     # Rest of code here
+    pass
